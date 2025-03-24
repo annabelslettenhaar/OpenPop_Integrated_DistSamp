56163f7b124b42da5e1ea5268139c9c9cb6ed79a
annabelslettenhaar <annabel.slettenhaar@live.nl>
2025-03-24 10:11:37 +0000

    Prevent error by smoothly creating new directory

--- R/plotMCMCTraces.R
+++ R/plotMCMCTraces.R
@@ -46,7 +46,7 @@ plotMCMCTraces <- function(mcmc.out, fitRodentCov, survVarT, VitalRates = TRUE, 
   
   # Vital rates
   if(VitalRates){
-    ifelse(!dir.exists("Plots/MCMCTraces"), dir.create("Plots/MCMCTraces"), FALSE) ## Check if folder exists, if not create folder
+    ifelse(!dir.exists("Plots/MCMCTraces"), dir.create("Plots/MCMCTraces", recursive = TRUE), FALSE) ## Check if folder exists, if not create folder
     pdf("Plots/MCMCTraces/MCMCtrace_mVR.pdf", width = 8, height = 6)
     MCMCvis::MCMCtrace(mcmc.out,
                        params = mVR_params,
