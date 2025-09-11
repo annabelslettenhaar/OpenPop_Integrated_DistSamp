a1bddcea5677e84bfd1d250fff1ae9bb0449672e
ChloeRNater <chloe.nater@nina.no>
2025-09-11 08:20:29 +0200

    Wrote catch to prevent probOcc = 1 in data likelihood
    The issue with infinite values in log(terrOcc) was caused by probOcc becoming = 1 early in the MCMC. This again was a consequence of ridiculously high values in totDens_std.
    
    I wrote a catch now that ensures that probOcc, when used in the data likelihood, is < 1. Note that this is "treating symptoms", not the root cause. Suboptimal initial values for density are likely the root cause.
    
    We can try to work with this "quick fix", but we need to screen our posteriors for ridiculously high density values.

--- R/writeModelCode_Integ.R
+++ R/writeModelCode_Integ.R
@@ -186,11 +186,11 @@ writeModelCode_Integ <- function(survVarT, telemetryData){
 
       # Gyrfalcon occupancy (per area)
       
-      for (t in 2:N_years){
+      for (t in 1:N_years){
         # terrOcc[x, t] = number of territories occupied per area
         # probOcc[x, t] = probability of occupancy
         # terrMonitoredOcc[x, t] = number of monitored territories for occupancy
-        terrOcc[x, t] ~ dbin(prob = probOcc[x, t], size = terrMonitoredOcc[x, t]) 
+        terrOcc[x, t] ~ dbin(prob = min(probOcc[x, t], 0.999), size = terrMonitoredOcc[x, t]) 
         
       } # t
       
