20db563cb8492ad188c2f6fb6b0e761f1abfe207
ChloeRNater <chloe.nater@nina.no>
2025-09-11 07:32:08 +0200

    Testing setup with productivity likelihood disabled
    I am working on finding the cause for infinite values in logProb_terrOcc and terrProd. I have disabled one of the likelihoods to approach the problem, and also only run one chain.

--- R/setupModel_Integ.R
+++ R/setupModel_Integ.R
@@ -41,9 +41,11 @@ setupModel_Integ <- function(modelCode, customDist,
               "sigmaT.Occ", "sigmaT.Prod", 
               "meanDens", "totDens_std",
               "probOcc", "terrProd",
+              "alphaPtar.Occ", "alphaPtar.Prod",
+              "betaPtar.Occ", "betaPtar.Prod", 
+              "sigmaT.Occ", "sigmaT.Prod",
               "Mu.D1", "sigma.D",
-              "S", "Mu.S", "betaGyr.S", 
-              "betaPtar.Occ", "betaPtar.Prod" 
+              "S", "Mu.S", "betaGyr.S"
               #"betaSD.S",
   )
   
