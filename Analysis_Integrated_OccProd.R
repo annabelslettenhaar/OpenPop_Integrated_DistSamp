1985b12073f098382e802ded45240e855097389e
annabelslettenhaar <annabel.slettenhaar@live.nl>
2025-10-22 12:57:10 +0000

    Add/change weather section to import based on ptarmigan transects. Import from memory after first download for efficiency

--- Analysis_Integrated_OccProd.R
+++ Analysis_Integrated_OccProd.R
@@ -96,9 +96,23 @@ d_rodent <- wrangleData_RodentGyr(#localities = localities,
 
 # WRANGLE WEATHER DATA #
 #----------------------#
-weather_data <- wrangleData_Weather(areas = areas,
-                                    areaAggregation = areaAggregation,
-                                    minYear = minYear, maxYear = maxYear)
+
+## Using gyrfalcon territory coordinates
+# weather_data <- wrangleData_Weather(areas = areas,
+#                                     areaAggregation = areaAggregation,
+#                                     minYear = minYear, maxYear = maxYear)
+
+
+## Using ptarmigan line transect coordinates
+d_temp <- wrangleData_Temp(minYear = minYear, 
+                           maxYear = maxYear, 
+                           areas = areas)
+
+saveRDS(d_temp, "data/weather/temp.rds")
+
+
+## Or import the ptarmigan weather data that is imported and processed earlier
+#d_temp <- readRDS("data/weather/temp.rds")
 
 
 # WRANGLE GYRFALCON DATA #
